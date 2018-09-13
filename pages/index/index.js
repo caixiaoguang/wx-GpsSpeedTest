7cbc4fc3ab812f683eafa1a6b2074934e2c68278
cxg <1023115946@qq.com>
2018-09-13 22:05:30 +0800

    map init

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,52 +3,109 @@
 const app = getApp()
 
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    lng: 113.324520,
+    lat: 23.099994,
+    scale: 17,
+    markers: [],
+    //轨迹
+    polyline: [],
+
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+
+  //地图中心移动到当前定位点
+  move2CurrentPos() {
+    this.addPoint2Polyline()
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      scale: 17
     })
+  },
+
+  //获取当前定位点并添加到polyline
+  addPoint2Polyline() {
+    let timeId = setInterval(res => {
+      wx.getLocation({
+        type: 'gcj02',
+        success: res => {
+          if (!this.data.polyline[0]) {
+            this.data.polyline[0] = {
+              points: []
+            }
+          }
+          this.data.polyline[0].points.push({
+            longitude: res.longitude,
+            latitude: res.latitude
+            
+          })
+          this.data.polyline[0].color = "#FF0000DD"
+          this.data.polyline[0].width = 4
+          this.setData({
+            lng: res.longitude,
+            lat: res.latitude,
+          })
+        },
+      })
+    }, 3000)
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+    this.addPoint2Polyline()
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
   }
 })
